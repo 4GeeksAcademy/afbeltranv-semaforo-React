8e65831bb545cbeffb79aee667632f32197c7ff1
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-05-09 02:36:29 +0000

    Se agrega semáforo con funcionalidad de cambio de color. Falta modular que los colores aparezcan siempre y lo que cambie sea el glow o toggle de selección.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,18 +5,42 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [glow, setGlow]=useState("red");
+	const [glowRojo, setGlowRojo]=useState("");
+	const [glowAmarillo, setGlowAmarillo]=useState("");
+	const [glowVerde, setGlowVerde]=useState("");
+
+	function clickRojo(){
+		console.log("Funciono")
+		setGlowRojo("red")
+		setGlowAmarillo("")	
+		setGlowVerde("")
+	}
+	function clickAmarillo(){
+		console.log("Funciono")
+		setGlowAmarillo("yellow")
+		setGlowRojo("")	
+		setGlowVerde("")
+	}
+	function clickVerde(){
+		console.log("Funciono")
+		setGlowVerde("green")
+		setGlowRojo("")
+		setGlowAmarillo("")	
+	}
 	return (
 		<div className="text-center">
-			<div>
-				<div>palito</div>
-				<div style={{backgroundColor:glow}}>rojo</div>
-				<div>amarillo</div>
-				<div>verde</div>
+			<div className="palito"></div>
+			<div className="marcoSemaforo">	
+				<div className="spacer"></div>			
+				<div className="luzSemaforo" onClick={clickRojo} style={{backgroundColor:glowRojo}}></div>
+				<div className="luzSemaforo" onClick={clickAmarillo} style={{backgroundColor:glowAmarillo}}></div>
+				<div className="luzSemaforo" onClick={clickVerde} style={{backgroundColor:glowVerde}}></div>
+				<div className="spacer"></div>	
 			</div>
 			
 		</div>
 	);
+	
 };
 
 export default Home;
