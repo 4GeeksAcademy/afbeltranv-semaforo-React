41d3edf2d8e6fc974fb09d390f8a41beb276c9ec
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-05-11 01:52:17 +0000

    Se agrega funcionalidad de ciclo de luces

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,23 +9,29 @@ const Home = () => {
 	const [glowAmarillo, setGlowAmarillo]=useState("");
 	const [glowVerde, setGlowVerde]=useState("");
 
-	function clickRojo(){
-		console.log("Funciono")
+	function clickRojo(){		
 		setGlowRojo("0 0 30px 10px white")
 		setGlowAmarillo("")	
 		setGlowVerde("")
 	}
-	function clickAmarillo(){
-		console.log("Funciono")
+	function clickAmarillo(){		
 		setGlowAmarillo("0 0 30px 10px white")
 		setGlowRojo("")	
 		setGlowVerde("")
 	}
-	function clickVerde(){
-		console.log("Funciono")
+	function clickVerde(){		
 		setGlowVerde("0 0 30px 10px white")
 		setGlowRojo("")
 		setGlowAmarillo("")	
+	}
+	function cambiarLuces(){		
+		setTimeout(clickRojo,500)
+		setTimeout(clickAmarillo,1000)
+		setTimeout(clickVerde,1500)
+	}
+
+	function cicloLuces(){
+		setInterval(cambiarLuces,2000)
 	}
 	return (
 		<div className="text-center">
@@ -37,6 +43,7 @@ const Home = () => {
 				<div className="luzSemaforoVerde" onClick={clickVerde} style={{boxShadow:glowVerde}}></div>
 				<div className="spacer"></div>	
 			</div>
+			<button onClick={cicloLuces}>Cambiar Luces</button>
 			
 		</div>
 	);
